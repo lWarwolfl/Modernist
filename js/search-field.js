a5a49b8d1e9d7a1ec1d1214bd8f375d4ba32e217
lWarwolfl <m.sinakheiry2@gmail.com>
2021-11-19 23:19:19 +0330

    Modernist v1.1

---
+++ js/search-field.js
@@ -0,0 +1,18 @@
+function expand_search() {
+    $('.top-nav-link').css('display', 'none');
+    $('.top-search-bar').css('width', '560px');
+}
+document.addEventListener("click", (evt) => {
+    const bar = document.getElementById("top-search-bar");
+    const button = document.getElementById("top-search-expand");
+    let targetElement = evt.target; // clicked element
+
+    do {
+        if (targetElement == bar || targetElement == button) {
+            return;
+        }
+        targetElement = targetElement.parentNode;
+    } while (targetElement);
+    $('.top-nav-link').css('display', 'inline-block');
+    $('.top-search-bar').css('width', '0');
+});
