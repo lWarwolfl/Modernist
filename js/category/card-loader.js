f7949cf8ef44c474d4151f72eede0c7e08c9011e
lWarwolfl <m.sinakheiry2@gmail.com>
2021-12-07 22:08:50 +0330

    Modernist v3.3

--- js/category/card-loader.js
+++ js/category/card-loader.js
@@ -4,8 +4,12 @@ $.getJSON('js/category/card-data.json', function(data){
         $('#category-card-container').append(
             '               <a href="category.html?category=Recent" class="category-card" style="background-image: url(\'image/category-square/' + data['card'][i]['image'] + '\')">\n' +
             '                    <div class="overlay">\n' +
-            '                        <div class="name">' + data['card'][i]['category'] + '</div>\n' +
-            '                        <div class="counter">' + data['card'][i]['count'] + '</div>\n' +
+            '                        <div class="icon-container">\n' +
+            '                           <i class="fas ' + data['card'][i]['icon'] + '"></i>\n' +
+            '                        </div>\n' +
+            '                        <div class="name">' + data['card'][i]['category'] + '\n' +
+            '                           <div class="counter">' + data['card'][i]['count'] + '</div>\n' +
+            '                        </div>\n' +
             '                    </div>\n' +
             '                </a>\n'
         );
